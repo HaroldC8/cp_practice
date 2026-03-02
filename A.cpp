71e741928ccabf4395c085ec93a696c0f9778666
HaroldC8 <pogojus.haroldas@gmail.com>
2026-03-02 02:01:15 +0200

    Boilerplate code

---
+++ A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//#define MULTI
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    
+}
+
+int main(int argc, char **argv)
+{    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    #ifdef MULTI
+        int t;
+        cin >> t;
+        while (t--)
+        {
+            solve();   
+        }
+    #else
+        solve();
+    #endif
+    return 0;
+}
